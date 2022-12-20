3721a25ca0e5e247effdee650895ce4dcc364650
GulenayMer <mervegulenay@gmail.com>
2022-12-20 20:43:42 +0100

    vector

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@
 /*   By: mgulenay <mgulenay@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 14:56:00 by mgulenay          #+#    #+#             */
-/*   Updated: 2022/12/16 14:56:01 by mgulenay         ###   ########.fr       */
+/*   Updated: 2022/12/20 16:08:02 by mgulenay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "vector.hpp"
+
+int main()
+{
+	
+}
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: mgulenay <mgulenay@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 14:56:11 by mgulenay          #+#    #+#             */
-/*   Updated: 2022/12/19 17:58:16 by mgulenay         ###   ########.fr       */
+/*   Updated: 2022/12/20 20:40:34 by mgulenay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,6 @@
 
 namespace ft
 {
-	
 	/* a generic class template vector */
 	template < class T, class Alloc = std::allocator<T> > 
 	class vector
@@ -52,9 +51,9 @@ namespace ft
 		protected:
 		
 		allocator_type	_allocType;
-		pointer 		_pointer; // pointer to allocated uninitialized storage
-		size_type 		_length;
-		size_type 		_capasity;
+		pointer 		_pointer_Arr; // pointer to allocated uninitialized storage
+		size_type 		_size; // number of actual objs stored
+		size_type 		_capasity; // storage capasity
 
 		
 		public: 
@@ -74,60 +73,150 @@ namespace ft
 		typedef typename allocator_type::const_reverse_iterator	const_reverse_iterator;
  */
 
-		/* Constructs a vector, 
-			initializing its contents depending on the constructor version used : */
+		/* ####################    CONSTRUCTORS  #############################
+			constructs a vector, initializing its contents depending on the constructor version used : */
 			
-		//default constructor : the container keeps & uses an internal copy
-		// of this allocator -- an alias to the 2. template parameter
-		explicit vector (const allocator_type &alloc = allocator_type()) 
-			: _allocType(alloc), _pointer(NULL), _length(0), _capasity(0)
-		{
-			std::cout << GREEN << " (default) constructor called " << RESET << std::endl;
-		}; 
-
-		/* constructor with parameters :
-			Constructs a container with n elements. Each element is a copy of val. */
+		//default constructor : the container keeps & uses an internal copy // of this allocator -- an alias to the 2. template parameter
+		explicit vector (const allocator_type &alloc = allocator_type())
+			: 	_allocType(alloc),  
+				_pointer_Arr(NULL),
+			 	_size(0),
+			  	_capasity(0)
+		{ std::cout << GREEN << " (default) constructor called " << RESET << std::endl; };
+		 
+		/* constructor with parameters : Constructs a container with n elements. Each element is a copy of val. */
 		explicit vector (size_type n, const value_type &val = value_type(), 
 						const allocator_type &alloc = allocator_type())
-			: _allocType(alloc), _pointer(NULL), _length(n), _capasity(n)
+			: 	_allocType(alloc), 
+				_pointer_Arr(NULL), 
+				_size(n), 
+				_capasity(n)
 		{
-			std::cout << GREEN << " constructor with n elements called " << RESET << std::endl;
+			//std::cout << GREEN << " constructor with n elements called " << RESET << std::endl;
+			this->_pointer_Arr = _allocType.allocate(n);
+			for (size_t i = 0; i < this->_size; i++)
+				this->_pointer_Arr[i] = val;
 			
-			/* pointer allocate (size_type n, allocator<void>::const_pointer hint=0);
-				Attempts to allocate a block of storage with a size large enough to contain 
-				n elements of member type value_type (an alias of the allocator's template 
-				parameter), and returns a pointer to the first element. */
-			this->_pointer = _allocType.allocate(n);
-			for (size_t i = 0; i < this->_length; i++)
-				this->_pointer[i] = val;
-				 
-
-		// void construct ( pointer p, const_reference val ) :
-			// Constructs an element object on the location pointed by p.
 		};
 	
 		// copy constructor 
-		vector (const vector &x): _pointer(0), _length(0), _capasity(0), _allocType(x._allocType)
+		vector (const vector &x)
+			: 	_allocType(x._allocType), 
+				_pointer_Arr(x._pointer_Arr), 
+				_size(x.size), 
+				_capasity(x.capacity)
 		{
-			std::cout << GREEN << " copy constructor called " << RESET << std::endl;
-			*this = x;
+			//std::cout << GREEN << " copy constructor called " << RESET << std::endl;
+			for (int i = 0; i < x.size(); i++)
+				_pointer_Arr[i] = x._pointer_Arr[i];
 		}
 
+		// 
+		vector& operator=( const vector& other );
+		
+		allocator_type get_allocator() const;
 
-		// destructor
+		
+			/* ########################## DESTRUCTOR  ##############################  */
 		~vector()
 		{
 			std::cout << RED << " destructor called " << RESET << std::endl;
-			delete[] this->_pointer;
-		}
 	
+		}
+
+		/* ###########################     ITERATORS  ##############################  */
+		iterator begin();
+		iterator end();
+		reverse_iterator rbegin();
+		reverse_iterator rend();
+		
+		/* ##########################     ELEMENT ACCESS   ##############################  */
+		reference at( size_type pos );
+		reference operator[]( size_type pos );
+		reference front();
+		reference back();
+		T* data();
+
+		/* ############################   CAPASITY   ##############################  */
+		bool		empty() const {  return (this->size) == 0 };
+		size_type	size() const { return (this->size) };
+		size_type	max_size() const;
+		void		reserve(size_type new_cap);
+		size_type	capacity() const { return (this->capacity) };
+		
+
+		/* ##########################     MODIFIERS / METHODS   ##############################  */
+		void clear();
+		iterator insert( const_iterator pos, const T& value );
+		iterator erase( iterator pos );
+		push_back(const T &value);
+		void pop_back();
+		void resize( size_type count );
+		void swap(vector &other);
 	}
+
+
+	/* ##########################     NON-MEMBER FUNCTIONS   ##############################  */
+	template< class T, class Alloc >
+	// true if the contents of the vectors are equal, false otherwise
+	bool operator==(const ft::vector<T,Alloc>& lhs,
+                	const ft::vector<T,Alloc>& rhs)
+	{
+		if (lhs.size() != rhs.size())
+			return false;
+		for (size_t i = 0; i < lhs.size(); i++)
+		{
+			if (lhs[i] != rhs[i])
+				return false;
+		}
+		return true;
+	}
+
+	template< class T, class Alloc >
+	// return true if the contents of the vectors are not equal, false otherwise
+	bool operator!=( const ft::vector<T,Alloc>& lhs,
+                	const ft::vector<T,Alloc>& rhs )
+	{
+		if ( !(lhs == rhs) )
+			return (true);
+		return (false);
+	}
+
+	template< class T, class Alloc >
+	// true if the contents of the lhs are lexicographically less than the contents of rhs, false otherwise
+	bool operator<( const ft::vector<T,Alloc>& lhs,
+                	const ft::vector<T,Alloc>& rhs );
+	
+	template< class T, class Alloc >
+	// true if the contents of the lhs are lexicographically less than or equal to the contents of rhs, false otherwise
+	bool operator<=( const ft::vector<T,Alloc>& lhs,
+                	const ft::vector<T,Alloc>& rhs );
+	
+	template< class T, class Alloc >
+	// true if the contents of the lhs are lexicographically greater than the contents of rhs, false otherwise
+	bool operator>( const ft::vector<T,Alloc>& lhs,
+                	const ft::vector<T,Alloc>& rhs );
+
+	template< class T, class Alloc >
+	// true if the contents of the lhs are lexicographically greater than or equal to the contents of rhs, false otherwise
+	bool operator>=( const ft::vector<T,Alloc>& lhs,
+                	const ft::vector<T,Alloc>& rhs );
+	
 }
 
 
 
+// to dos :
+// increase the capasity of the vector to a value that is greate or
+// equal to new_cap  : void reserve(size_type new_cap);
 
 
+/* pointer allocate (size_type n, allocator<void>::const_pointer hint=0);
+				Attempts to allocate a block of storage with a size large enough to contain 
+				n elements of member type value_type (an alias of the allocator's template 
+				parameter), and returns a pointer to the first element. */
 
+// void construct ( pointer p, const_reference val ) :
+			// Constructs an element object on the location pointed by p.
 
 #endif
