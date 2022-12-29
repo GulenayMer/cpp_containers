fc4ad4750ac25e7848c06c65427b78ccd08c795c
GulenayMer <mervegulenay@gmail.com>
2022-12-29 15:46:57 +0100

    std func

--- iterator.hpp
+++ iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: mgulenay <mgulenay@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 17:16:43 by mgulenay          #+#    #+#             */
-/*   Updated: 2022/12/28 20:12:19 by mgulenay         ###   ########.fr       */
+/*   Updated: 2022/12/29 11:55:28 by mgulenay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -259,16 +259,16 @@ namespace ft
 			relative to the element they point to, offering the same functionality as pointers
 		*/
 	template <typename T>
-	class random_access_iterator
+	class random_access_iterator : public iterator<random_access_iterator_tag, T>
 	{
 
 		public:
 		
-		typedef typename iterator<random_access_iterator_tag, T>::value_type		value_type;
-		typedef typename iterator<random_access_iterator_tag, T>::difference_type	difference_type;
-		typedef typename iterator<random_access_iterator_tag, T>::pointer			pointer;
-		typedef typename iterator<random_access_iterator_tag, T>::reference			reference;
-		typedef typename iterator<random_access_iterator_tag, T>::iterator_category	iterator_category;
+		typedef typename ft::iterator<random_access_iterator_tag, T>::value_type		value_type;
+		typedef typename ft::iterator<random_access_iterator_tag, T>::difference_type	difference_type;
+		typedef typename ft::iterator<random_access_iterator_tag, T>::pointer			pointer;
+		typedef typename ft::iterator<random_access_iterator_tag, T>::reference			reference;
+		typedef typename ft::iterator<random_access_iterator_tag, T>::iterator_category	iterator_category;
 
 		protected: 
 			pointer		_it_ptr;
@@ -276,24 +276,151 @@ namespace ft
 		public:
 		
 		random_access_iterator() : _it_ptr(NULL) {};
-
 		random_access_iterator(pointer ptr) : _it_ptr(ptr) {};
-		
 		random_access_iterator(const random_access_iterator<T> &src) : _it_ptr(src._it_ptr) {};
-
 		random_access_iterator &operator=(const random_access_iterator<T> &src)
 		{
 			if (this != src)
 				_it_ptr = src._it_ptr;
 			return (*this);
-		};
-			
+		};	
 		virtual ~random_access_iterator() {};
-	}
 
+		pointer base() const
+		{
+			return (_it_ptr);
+		}
+
+		/* ------------------------------ reference & dereference -------------------- */
+		reference operator*() const
+		{
+			return (*_it_ptr);
+		}		
+		
+		pointer operator->() const
+		{
+			return _it_ptr;
+		}
+	
+		/* dereference with offset */
+		reference operator[](difference_type n) const
+		{
+			return (_it_ptr + n);
+		}
+	
+		/*  --------------------- increment & decrement ----------------------- */
+		random_access_iterator operator++(int)
+		{
+			random_access_iterator temp(*this);
+			_it_ptr += 1;
+			return (temp);
+		}
+
+		random_access_iterator &operator++()
+		{
+			++_it_ptr;
+			return (*this);
+		}
+
+		random_access_iterator operator--(int)
+		{
+			random_access_iterator temp(*this);
+			_it_ptr -= 1;
+			return (temp);
+		}
+
+		random_access_iterator &operator--()
+		{
+			--_it_ptr;
+			return (*this);
+		}
+
+		/* ------------------- arithmetic operators --------------------- */
+
+		/* returns a random access iterator pointing to the element located n positions away from the element the 
+			iterator currently points to
+		*/
+		random_access_iterator operator+(difference_type n) const
+		{
+			return (_it_ptr + n);	
+		}
+	
+		/* returns a reverse iterator pointing to the element located n positions before the element the 
+			iterator currently points to
+		*/
+		random_access_iterator operator-(difference_type n) const
+		{
+			return (_it_ptr - n);
+		}
+
+		/* advances the reverse_iterator by n elements positions */
+		random_access_iterator operator+=(difference_type n) const
+		{
+			_it_ptr += n;
+			return (*this);	
+		}
+
+		/* decreases the reverse_iterator by n elements positions */
+		random_access_iterator operator-=(difference_type n) const
+		{
+			_it_ptr -= n;
+			return (*this);	
+		}
+
+	};
+
+		/*    ################  non member function overloads for random access iterator ###################### */
+	
+	/* 	 Performs the appropriate comparison operation between the reverse_iterator objects lhs and rhs. */
+	template <class Iterator>
+	bool operator==(const ft::random_access_iterator<Iterator>& lhs, const ft::random_access_iterator<Iterator>& rhs)
+	{
+		return ( lhs.base() == rhs.base() );
+	};
+	
+	bool operator!=(const ft::random_access_iterator<Iterator>& lhs, const ft::random_access_iterator<Iterator>& rhs)
+	{
+		return ( lhs.base() != rhs.base() );
+	};
+	
+	bool operator<(const ft::random_access_iterator<Iterator>& lhs, const ft::random_access_iterator<Iterator>& rhs)
+	{
+		return ( lhs.base() < rhs.base() );
+	};
+	
+	bool operator<=(const ft::random_access_iterator<Iterator>& lhs, const ft::random_access_iterator<Iterator>& rhs)
+	{
+		return ( lhs.base() <= rhs.base() );
+	};
+	
+	bool operator>(const ft::random_access_iterator<Iterator>& lhs, const ft::random_access_iterator<Iterator>& rhs)
+	{
+		return ( lhs.base() > rhs.base() );
+	};
+
+	bool operator>=(const ft::random_access_iterator<Iterator>& lhs, const ft::random_access_iterator<Iterator>& rhs)
+	{
+		return ( lhs.base() >= rhs.base() );
+	};
+	
+	/* returns a reverse iterator pointing to the element located n positions away 
+		from the element pointed to by rev_iter 
+	*/
+	template <class Iterator>
+	random_access_iterator<iterator> operator+(typename ft::random_access_iterator<Iterator>::difference_type n,
+							const ft::random_access_iterator<Iterator>& rev_it)
+	{
+		return (rev_it + n);
+	};
+	
+	/* returns the distance between lhs & rhs */
+	template <class Iterator>  
+	typename random_access_iterator<Iterator>::difference_type operator-(const random_access_iterator<Iterator>& lhs,
+					const random_access_iterator<Iterator>& rhs)
+	{
+		return (lhs.base() - rhs.base());
+	};
 
 } 
-
-
 
 #endif
---
+++ std_functions.hpp
@@ -0,0 +1,252 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   std_functions.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgulenay <mgulenay@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 13:41:55 by mgulenay          #+#    #+#             */
+/*   Updated: 2022/12/29 15:42:58 by mgulenay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STD_FUNCTIONS_HPP
+#define STD_FUNCTIONS_HPP
+
+// <type_traits>
+#include <iostream>
+
+namespace ft
+{
+
+	/* ########################## ENABLE_IF ######################## */
+	
+	/* useful to hide signatures on compile time when a particular
+	condition is not met, since in this case, the member enavble_if::type
+	will not be defined & attempting to compile using it should fail 
+		- Cond is a compile-time constant of type bool.
+		- T, a type
+	*/
+	template <bool Cond, class T = void>
+	struct enable_if {};
+	
+	template <class T>
+	struct enable_if<true, T>
+	{
+		typedef T type;
+	};
+	
+	
+	/* ########################## IS_INTEGRAL ######################## */
+
+	/*  trait class that identifies whether T is an integral type
+		- template takes T as parameter
+		- value_type is bool
+		- type either true_type or false_type
+	 */
+	template <typename T> 
+	struct is_integral
+	{
+		static const bool value = false;
+	};
+
+	/*   ------ fundamental integral types -------  */
+	template <>
+	struct is_integral<bool>
+	{
+		static const bool value = true;
+	};
+	
+	template <>
+	struct is_integral<char>
+	{
+		static const bool value = true;
+	};
+	
+	template <>
+	struct is_integral<char16_t>
+	{
+		static const bool value = true;
+	};
+
+	template <>
+	struct is_integral<char32_t>
+	{
+		static const bool value = true;
+	};
+	
+	template <>
+	struct is_integral<wchar_t>
+	{
+		static const bool value = true;
+	};
+	
+	template <>
+	struct is_integral<signed char>
+	{
+		static const bool value = true;
+	};
+	
+
+	template <>
+	struct is_integral<short int>
+	{
+		static const bool value = true;
+	};
+
+
+	template <>
+	struct is_integral<int>
+	{
+		static const bool value = true;
+	};
+	
+	template <>
+	struct is_integral<long int>
+	{
+		static const bool value = true;
+	};
+
+	template <>
+	struct is_integral<long long int>
+	{
+		static const bool value = true;
+	};
+	
+	template <>
+	struct is_integral<unsigned char>
+	{
+		static const bool value = true;
+	};
+
+	template <>
+	struct is_integral<unsigned short int>
+	{
+		static const bool value = true;
+	};
+
+	template <>
+	struct is_integral<unsigned int>
+	{
+		static const bool value = true;
+	};
+
+	template <>
+	struct is_integral<unsigned long int>
+	{
+		static const bool value = true;
+	};
+
+	template <>
+	struct is_integral<unsigned long long int>
+	{
+		static const bool value = true;
+	};
+
+	template <typename T>
+	struct is_integral<const T> : is_integral<T> {};
+	
+	template <typename T>
+	struct is_integral<volatile T> : is_integral<T> {};
+
+	template <typename T>
+	struct is_integral<const volatile T> : is_integral<T> {};
+
+	
+	/* ########################## LEXICOGRAPHICAL_COMPARE ######################## */
+	
+	/* this comparasion is generally used to sort words alphabetically;
+		involves comparing sequentially the elements that have the same
+		position in both ranges against each other until one element is not
+		equivalent to the other. 
+		The result of these first non-matching elements is the result of the 
+		lexi.comparison.
+	*/
+	
+	/* 
+		- first1, last1 : Input iterators of the first sequence.
+		includes all elements btw f1 & l1 (except l1).
+		- first2, last2 : Input iterators to the initial & final positions
+		of the second sequence. 
+	*/
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+					InputIterator2 first2, InputIterator2 last2)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			first1 += 1;
+			first2 += 1;
+		}
+		return (first2 != last2);
+	};
+
+	/*
+	Binary function that accepts two arguments of the types pointed by the iterators, and
+	returns a value convertible to bool. The value returned indicates whether the first argument is 
+	considered to go before the second in the specific strict weak ordering it defines.
+	The function shall not modify any of its arguments.
+	This can either be a function pointer or a function object.
+	*/
+	template <class InputIterator1, class InputIterator2, class Compare> 
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, 
+								InputIterator2 first2, InputIterator2 last2, Compare comp)
+	{
+		while (first1 != last1)
+		{
+			if (comp(*first1, *first2))
+				return false;
+			else if (comp(*first2, *first1))
+				return true;
+			first1 += 1;
+			first2 += 1;
+		}
+		return (first2 != last2);					
+	};
+
+
+	/* ##########################  EQUAL ######################## */
+
+	/*
+		compares the elements in the range f1, l1 with those in the range beginning
+		at f2 & returns true if all of the elements in both ranges match.
+	*/
+	template <class InputIterator1, class InputIterator2> 
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+	{
+		while (first1 != last1)
+		{
+			if ( !(*first1 == *first2) )
+				return false;
+			++first1;
+			++first2;
+		}
+		return true;
+	};
+
+	/*
+	Binary function that accepts two elements as argument 
+	(one of each of the two sequences, in the same order), and returns a value convertible to bool. 
+	The value returned indicates whether the elements are considered to match in the context of this func.
+	The function shall not modify any of its arguments.
+	This can either be a function pointer or a function object.
+	*/
+	template <class InputIterator1, class InputIterator2, class BinaryPredicate>  
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
+	{
+		while (first1 != last1)
+		{
+			if ( pred(*first1, *first2) == false)
+				return false;
+			++first1;
+			++first2;
+		}
+		return true;
+	};
+} 
+
+#endif
